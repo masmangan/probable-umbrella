3abf1a74ff0bb717437b97243f9d3b359e29ef49
Marco Mangan <marco.mangan@pucrs.br>
2025-05-21 11:39:47 -0300

    Initial sharing of project

---
+++ TicTacToe.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TicTacToe
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Jogo da Velha!");
+        // // variaveis
+        // Scanner in;
+        // int a;
+        // int b;
+        // int s;
+
+        // // entrada de dados
+        // in = new Scanner(System.in);
+        // a = in.nextInt();      
+        // b = in.nextInt();      
+        // in.close();
+
+        // // processamento de dados
+        // s = a + b;
+
+        // // saida de dados
+        // System.out.println(s);
+    }
+}
