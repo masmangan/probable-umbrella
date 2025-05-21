642bc18825acd640f925d33c5d39e01a1367a1ad
Marco Mangan <marco.mangan@pucrs.br>
2025-05-21 12:06:08 -0300

    Closes #1

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,6 +5,18 @@ public class TicTacToe
     public static void main(String[] args)
     {
         System.out.println("Jogo da Velha!");
+        char[][] grid = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}        
+            };
+
+        System.out.printf("%c|%c|%c\n", grid[0][0], grid[0][1],grid[0][2]);
+        System.out.printf("-+-+-\n");
+        System.out.printf("%c|%c|%c\n", grid[1][0], grid[1][1],grid[1][2]);
+        System.out.printf("-+-+-\n");
+        System.out.printf("%c|%c|%c\n", grid[2][0], grid[2][1],grid[2][2]);
+
         // // variaveis
         // Scanner in;
         // int a;
